9076fbefb97a490efe86b535c9890e5bbe8e5041
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-07-14 17:59:24 +0500

    Corrected mistake inside baseEnitity

--- Core/Entities/BaseEntity.cs
+++ Core/Entities/BaseEntity.cs
@@ -1,15 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Core.Entities
 {
     public class BaseEntity
     {
         public int Id { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public string PictureUrl { get; set; }
-        public ProductType ProductType { get; set; }
-        public int ProductTypeId { get; set; }
-        public ProductBrand ProductBrand { get; set; }
-        public int ProductBrandId { get; set; }
-        
     }
 }
