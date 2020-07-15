cb0c324622cb34fdd40c2e55fe0838cea2ff5e23
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-07-15 11:36:36 +0500

    Added swagger for documentation

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,6 +1,6 @@
+using API.Extensions;
 using API.Helpers;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -24,12 +24,13 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddControllers();
+            services.AddApplicationServices();
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddAutoMapper(typeof(MappingProfiles));
+            services.AddSwaggerDocumentation();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +47,7 @@ namespace API
             app.UseStaticFiles();
 
             app.UseAuthorization();
-
+            app.UseSwaggerDocumentation();
             app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
